11d9e6a4752dfcff5b0c4d75e71953cd150efbf8
ned <>
2010-03-27 10:23:37 +0000

    all colour ranges working

--- colourtoy.js
+++ colourtoy.js
@@ -50,20 +50,45 @@ updateHSL = function (event) {
 };
 
 updateColourRanges = function (h, s, l) {
-    var c = new Colour({h: 0, s:0, l: l});
+    var i, ctx, grad, c = new Colour({h: 0, s:0, l: l});
     // hue
-    $("#h-full-spectrum-shading").css({
-        "background-color": c.toString(),
-        opacity: Math.max(1 - s, Math.abs(0.5 - l) * 2)
-    });
+    ctx = document.getElementById('colour-range-h').getContext('2d');
+    grad = ctx.createLinearGradient(0,0,400,0);
+    for (i=0; i < 7; i++) {
+        grad.addColorStop(i * (1/6), new Colour({h: i * (1/6), s:s, l:l}).toString());
+    }
+    ctx.fillStyle = grad;
+    ctx.fillRect(0,0,400,20);
     // saturation
-    $("#s-base-colour").css("background-color", new Colour({h: h, s: 1, l: 0.5}).toString());
-    $("#s-shading").css({
-        "background-color": (l < 0.5)?"#000":"#fff",
-        opacity: Math.abs(0.5 - l) * 2
-    });
+    ctx = document.getElementById('colour-range-s').getContext('2d');
+    grad = ctx.createLinearGradient(0,0,400,0);
+    grad.addColorStop(0, new Colour({h: h, s:0, l:l}).toString());
+    grad.addColorStop(1, new Colour({h: h, s:1, l:l}).toString());
+    ctx.fillStyle = grad;
+    ctx.fillRect(0,0,400,20);
     //lightness
-    $("#l-base-colour").css("background-color", new Colour({h: h, s: s, l: 0.5}).toString());    
+    ctx = document.getElementById('colour-range-l').getContext('2d');
+    grad = ctx.createLinearGradient(0,0,400,0);
+    grad.addColorStop(0, '#000');
+    grad.addColorStop(0.5, new Colour({h: h, s: s, l: 0.5}).toString());
+    grad.addColorStop(1, '#fff');
+    ctx.fillStyle = grad;
+    ctx.fillRect(0,0,400,20);
+    // red
+    var components = ["r", "g", "b"];
+    var rgb = Colour.hslToRGB({h:h, s:s, l:l});
+    for (i=0; i < 3; i++) {
+        component = components[i];
+        ctx = document.getElementById('colour-range-' + component).getContext('2d');
+        grad = ctx.createLinearGradient(0,0,400,0);
+        rgb2 = $.extend({}, rgb);
+        rgb2[component] = 0;
+        grad.addColorStop(0, new Colour(rgb2).toString());
+        rgb2[component] = 255;
+        grad.addColorStop(1, new Colour(rgb2).toString());
+        ctx.fillStyle = grad;
+        ctx.fillRect(0,0,400,20);        
+    }
 };
 
 
