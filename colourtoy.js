9822e71b3749a063a432f8f0fa3fb0c315a09c5b
ned <>
2010-03-30 22:08:11 +0100

    moved canvas bars into sliders, and added a spinner control

--- colourtoy.js
+++ colourtoy.js
@@ -1,6 +1,6 @@
 (function ($) {
     
-var updateSwatch;
+var updateSwatch, needUpdate = false;
 
 $(function () {
     var opts = {
@@ -19,6 +19,7 @@ $(function () {
         slide: updateHSL,
         stop: updateHSL
     }));
+    $("#input-r").spinner({min:0, max: 255});
 });    
 
 
@@ -49,44 +50,49 @@ updateHSL = function (event) {
 
 updateColourRanges = function (h, s, l) {
     var i, ctx, grad, c = new Colour({h: 0, s:0, l: l});
-    // hue
-    ctx = document.getElementById('colour-range-h').getContext('2d');
-    grad = ctx.createLinearGradient(0,0,400,0);
-    for (i=0; i < 7; i++) {
-        grad.addColorStop(i * (1/6), new Colour({h: i * (1/6), s:s, l:l}).toString());
-    }
-    ctx.fillStyle = grad;
-    ctx.fillRect(0,0,400,20);
-    // saturation
-    ctx = document.getElementById('colour-range-s').getContext('2d');
-    grad = ctx.createLinearGradient(0,0,400,0);
-    grad.addColorStop(0, new Colour({h: h, s:0, l:l}).toString());
-    grad.addColorStop(1, new Colour({h: h, s:1, l:l}).toString());
-    ctx.fillStyle = grad;
-    ctx.fillRect(0,0,400,20);
-    //lightness
-    ctx = document.getElementById('colour-range-l').getContext('2d');
-    grad = ctx.createLinearGradient(0,0,400,0);
-    grad.addColorStop(0, '#000');
-    grad.addColorStop(0.5, new Colour({h: h, s: s, l: 0.5}).toString());
-    grad.addColorStop(1, '#fff');
-    ctx.fillStyle = grad;
-    ctx.fillRect(0,0,400,20);
-    // red
-    var components = ["r", "g", "b"];
-    var rgb = Colour.hslToRGB({h:h, s:s, l:l});
-    for (i=0; i < 3; i++) {
-        component = components[i];
-        ctx = document.getElementById('colour-range-' + component).getContext('2d');
+    needUpdate = true;
+    setTimeout(function () {
+        if ( ! needUpdate) return;
+        // hue
+        ctx = document.getElementById('colour-range-h').getContext('2d');
         grad = ctx.createLinearGradient(0,0,400,0);
-        rgb2 = $.extend({}, rgb);
-        rgb2[component] = 0;
-        grad.addColorStop(0, new Colour(rgb2).toString());
-        rgb2[component] = 255;
-        grad.addColorStop(1, new Colour(rgb2).toString());
+        for (i=0; i < 7; i++) {
+            grad.addColorStop(i * (1/6), new Colour({h: i * (1/6), s:s, l:l}).toString());
+        }
         ctx.fillStyle = grad;
-        ctx.fillRect(0,0,400,20);        
-    }
+        ctx.fillRect(0,0,400,20);
+        // saturation
+        ctx = document.getElementById('colour-range-s').getContext('2d');
+        grad = ctx.createLinearGradient(0,0,400,0);
+        grad.addColorStop(0, new Colour({h: h, s:0, l:l}).toString());
+        grad.addColorStop(1, new Colour({h: h, s:1, l:l}).toString());
+        ctx.fillStyle = grad;
+        ctx.fillRect(0,0,400,20);
+        //lightness
+        ctx = document.getElementById('colour-range-l').getContext('2d');
+        grad = ctx.createLinearGradient(0,0,400,0);
+        grad.addColorStop(0, '#000');
+        grad.addColorStop(0.5, new Colour({h: h, s: s, l: 0.5}).toString());
+        grad.addColorStop(1, '#fff');
+        ctx.fillStyle = grad;
+        ctx.fillRect(0,0,400,20);
+        // red
+        var components = ["r", "g", "b"];
+        var rgb = Colour.hslToRGB({h:h, s:s, l:l});
+        for (i=0; i < 3; i++) {
+            component = components[i];
+            ctx = document.getElementById('colour-range-' + component).getContext('2d');
+            grad = ctx.createLinearGradient(0,0,400,0);
+            rgb2 = $.extend({}, rgb);
+            rgb2[component] = 0;
+            grad.addColorStop(0, new Colour(rgb2).toString());
+            rgb2[component] = 255;
+            grad.addColorStop(1, new Colour(rgb2).toString());
+            ctx.fillStyle = grad;
+            ctx.fillRect(0,0,400,20);        
+        }
+        needUpdate = false;
+    }, 0);
 };
 
 
