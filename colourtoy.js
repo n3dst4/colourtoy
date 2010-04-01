8b3ee995060bd6a3e0cc4db580e12f9bb693c973
ned <>
2010-04-01 23:27:27 +0100

    made some fairly major optimisations. movement is now a bit wibbly but the slider movement is much smoother in IE.

--- colourtoy.js
+++ colourtoy.js
@@ -19,7 +19,7 @@ $.widget("ui.colourSlider", {
             max: this.options.scale,
             step: this.options.step,
             slide: updateFromSlider,
-            stop:  updateFromSlider
+            change:  updateFromSlider
         }).data("slider");
         
         this.canvas = $("<canvas/>").attr({
@@ -37,18 +37,18 @@ $.widget("ui.colourSlider", {
         this.input.change(updateFromInput);
     },
     update: function (colour) {
+        var stops = this.options.getGradient(colour);
         this.slider.value(this.options.getValue(colour));
         this.input.val(this.options.getValue(colour));
         //draw gradient
-        var stops = this.options.getGradient(colour);
-        var ctx = this.canvas[0].getContext('2d');
-        var grad = ctx.createLinearGradient(0,0,400,0);
+        this.ctx = this.ctx || this.canvas[0].getContext('2d');
+        var grad = this.ctx.createLinearGradient(0,0,400,0);
         for (i=0; i < stops.length; i++) {
             if (stops[i].toString().length > 7) debugger;
             grad.addColorStop(i * (1/(stops.length-1)), stops[i].toString());
         }
-        ctx.fillStyle = grad;
-        ctx.fillRect(0,0,400,20);
+        this.ctx.fillStyle = grad;
+        this.ctx.fillRect(0,0,400,20);
 
     }
 });
@@ -59,21 +59,31 @@ $.ui.colourSlider.defaults = {
 
 
 $(function () {
-    var colour = Colour("#eb2704"),
-        updateAll = function () {
-            $("#main-swatch").css({
-                "background-color": colour.toString(),
-                color: colour.contrast()
-            }).html(colour.toString());
-            $("#r-slider").colourSlider("update", colour);
-            $("#g-slider").colourSlider("update", colour);
-            $("#b-slider").colourSlider("update", colour);
-            $("#h-slider").colourSlider("update", colour);
-            $("#s-slider").colourSlider("update", colour);
-            $("#l-slider").colourSlider("update", colour);
-        };
+    var rSlider, gSlider, bSlider, hSlider, sSlider, lSlider,
+        mainSwatch = $("#main-swatch"),
+        needUpdate = false,
+        colour = Colour("#eb2704");
+        
+    function updateAll () {
+        if ( ! needUpdate) {
+            needUpdate = true;
+            setTimeout(function () {
+                needUpdate = false;
+                mainSwatch.css({
+                    "background-color": colour.toString(),
+                    color: colour.contrast()
+                }).html(colour.toString());
+                rSlider.update(colour);
+                gSlider.update(colour);
+                bSlider.update(colour);
+                hSlider.update(colour);
+                sSlider.update(colour);
+                lSlider.update(colour);
+            }, 0);
+        }
+    }
     
-    $("#r-slider").colourSlider({
+    rSlider = $("#r-slider").colourSlider({
         scale: 255,
         step: 1,
         getGradient: function (col) { return [col.red(0), col.red(255)]; },
@@ -82,9 +92,9 @@ $(function () {
             colour = colour.red(value);
             updateAll();
         }
-    });
+    }).data("colourSlider");
     
-    $("#g-slider").colourSlider({
+    gSlider = $("#g-slider").colourSlider({
         scale: 255,
         step: 1,
         getGradient: function (col) { return [col.green(0), col.green(255)]; },
@@ -93,9 +103,9 @@ $(function () {
             colour = colour.green(value);
             updateAll();
         }
-    });
+    }).data("colourSlider");
     
-    $("#b-slider").colourSlider({
+    bSlider = $("#b-slider").colourSlider({
         scale: 255,
         step: 1,
         getGradient: function (col) { return [col.blue(0), col.blue(255)]; },
@@ -104,9 +114,9 @@ $(function () {
             colour = colour.blue(value);
             updateAll();
         }
-    });
+    }).data("colourSlider");
     
-    $("#h-slider").colourSlider({
+    hSlider = $("#h-slider").colourSlider({
         scale: 1,
         step: 0.01,
         getGradient: function (col) {
@@ -124,9 +134,9 @@ $(function () {
             colour = colour.hue(value);
             updateAll();
         }
-    });
+    }).data("colourSlider");
     
-    $("#s-slider").colourSlider({
+    sSlider = $("#s-slider").colourSlider({
         scale: 1,
         step: 0.01,
         getGradient: function (col) {
@@ -137,103 +147,26 @@ $(function () {
             colour = colour.saturation(value);
             updateAll();
         }
-    });
+    }).data("colourSlider");
     
-    $("#l-slider").colourSlider({
+    lSlider = $("#l-slider").colourSlider({
         scale: 1,
         step: 0.01,
         getGradient: function (col) {
-            return [col.lightness(0), col.lightness(1)];
+            return [col.lightness(0), col.lightness(0.5), col.lightness(1)];
         },
         getValue: function (colour) { return colour.lightness(); },
         update: function (value) {
             colour = colour.lightness(value);
             updateAll();
         }
-    });
+    }).data("colourSlider");
     
     updateAll();
 
     
 });    
 
-
-updateRGB = function (event) {
-    var r = $("#r-slider").slider("value"),
-        g = $("#g-slider").slider("value"),
-        b = $("#b-slider").slider("value"),
-        c = new Colour({r: r, g: g, b: b});
-    $("#main-swatch").css("background", c.toString());
-    $("#h-slider").slider("value", c.h);
-    $("#s-slider").slider("value", c.s);
-    $("#l-slider").slider("value", c.l);
-    updateColourRanges(c.h, c.s, c.l);
-};
-
-updateHSL = function (event) {
-    var h = $("#h-slider").slider("value"),
-        s = $("#s-slider").slider("value"),
-        l = $("#l-slider").slider("value"),
-        c = new Colour({h: h, s: s, l: l}),
-        rgb = Colour.hslToRGB(c);
-    $("#main-swatch").css("background", c.toString());
-    $("#r-slider").slider("value", rgb.r);
-    $("#g-slider").slider("value", rgb.g);
-    $("#b-slider").slider("value", rgb.b);
-    updateColourRanges(h, s, l);
-};
-
-updateColourRanges = function (h, s, l) {
-    var i, ctx, grad, c = new Colour({h: 0, s:0, l: l});
-    needUpdate = true;
-    setTimeout(function () {
-        if ( ! needUpdate) return;
-        // hue
-        ctx = document.getElementById('colour-range-h').getContext('2d');
-        grad = ctx.createLinearGradient(0,0,400,0);
-        for (i=0; i < 7; i++) {
-            grad.addColorStop(i * (1/6), new Colour({h: i * (1/6), s:s, l:l}).toString());
-        }
-        ctx.fillStyle = grad;
-        ctx.fillRect(0,0,400,20);
-        // saturation
-        ctx = document.getElementById('colour-range-s').getContext('2d');
-        grad = ctx.createLinearGradient(0,0,400,0);
-        grad.addColorStop(0, new Colour({h: h, s:0, l:l}).toString());
-        grad.addColorStop(1, new Colour({h: h, s:1, l:l}).toString());
-        ctx.fillStyle = grad;
-        ctx.fillRect(0,0,400,20);
-        //lightness
-        ctx = document.getElementById('colour-range-l').getContext('2d');
-        grad = ctx.createLinearGradient(0,0,400,0);
-        grad.addColorStop(0, '#000');
-        grad.addColorStop(0.5, new Colour({h: h, s: s, l: 0.5}).toString());
-        grad.addColorStop(1, '#fff');
-        ctx.fillStyle = grad;
-        ctx.fillRect(0,0,400,20);
-        // red
-        var components = ["r", "g", "b"];
-        var rgb = Colour.hslToRGB({h:h, s:s, l:l});
-        for (i=0; i < 3; i++) {
-            component = components[i];
-            ctx = document.getElementById('colour-range-' + component).getContext('2d');
-            grad = ctx.createLinearGradient(0,0,400,0);
-            rgb2 = $.extend({}, rgb);
-            rgb2[component] = 0;
-            grad.addColorStop(0, new Colour(rgb2).toString());
-            rgb2[component] = 255;
-            grad.addColorStop(1, new Colour(rgb2).toString());
-            ctx.fillStyle = grad;
-            ctx.fillRect(0,0,400,20);        
-        }
-        needUpdate = false;
-    }, 0);
-};
-
-
-//        h = $("#h-slider").slider("value"),
-//        s = $("#s-slider").slider("value"),
-//        l = $("#l-slider").slider("value");
 
 
 }(jQuery));
